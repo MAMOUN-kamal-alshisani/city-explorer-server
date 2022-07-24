a6295309144ef7723c7aeacfeb8e31dd82a232e0
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2022-07-24 15:29:42 +0300

    some fixes

---
+++ readme.md
@@ -0,0 +1,12 @@
+# City Explorer !
+
+**city explorer is full stack web page that consists of :**
+
+1. **Server : Getting Back Modified API Data EndPoints**
+
+***a.Weather_Api: weather data for a specified City Entered using query request***
+***b.Movies_Api: returns data of a specific movie Name*** 
+
+**2.Technologies used : express, axios and crude opperations**
+
+
--- server.js
+++ server.js
@@ -1,61 +1,81 @@
-const express = require('express')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const PORT = process.env.PORT || 3000;
+const server = express();
+server.use(express.json());
+server.use(cors());
+// const weather = require('./data/weather.json')
+
+server.get("/", (req, res) => {
+  res.status(200).send(`<h1>Home Page</h1>`);
+});
+/// weather Api End Point + WeatherApi express Functionality
+server.get("/weather", GetWeatherApi);
+/// Movies Api End Point + MoviesApi express Functionality
+server.get("/movies", GetMoviesApi);
 
 
-require('dotenv').config()
-const PORT = process.env.PORT || 3000
-const server = express()
-
-const weather = require('./data/weather.json')
-
-server.use(express.json())
-server.use(cors())
-server.get('/',(req,res)=>{
-
-
-res.status(200).send(`<h1>Home Page</h1>`)
-
-})
-
-
-
-server.get('/weather',(req,res)=>{
-try{
-    let selectedData = weather.find(data=>{
-
-        if(data.city_name == req.query.cityName){
-// let weather = new ForeCast(data.data.datetime, data.data.description)
-console.log(req.query);
-console.log(data);
-
-return data
-        }
-    })
-    const weatherData = selectedData.data.map(forItem=>{
-
-
-        return new ForeCast(forItem.datetime,forItem.weather.description)
-    })
+///// Weather-Api express Functionality /////
+function GetWeatherApi(req, res) {
+  let cityQuery = req.query.cityWeather;
+  try {
     
-    
-    res.status(201).send(weatherData)
-
-
-}catch(err){console.log(err);}
-
-
-})
-
-class ForeCast{
-   
-    constructor(datetime,description){
-
-
-    this.datetime = datetime;
-    this.description = description;
-
-    }
+    //// https://api.weatherbit.io/v2.0/forecast/daily?city=$Amman&key=df63b6be79c7449887363eb55cd2eec1&days=6
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityQuery}&key=${process.env.WEATHER_API_KEY_SECOND}&days=6`;
+    axios.get(url).then((api) => {
+      let apiData = api.data.data.map((allData) => {
+        return new ForeCast(allData.valid_date, allData.weather.description);
+      });
+      console.log(apiData);
+      res.status(201).send(apiData);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
-server.listen(PORT,()=> console.log(`server Running on port ${PORT}`))
+///// Movies-Api express Functionality /////
+function GetMoviesApi(req, res) {
+  let cityName = req.query.cityName;
+  try {
+    // let moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=220df3e1b427989999446f230e82613d&query=${cityName}`;
+    let moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${cityName}`;
+    axios.get(moviesUrl).then((response) => {
+        console.log(response.data.results);
+      let allData = response.data.results.map((ApiData) => {
+        return new Movies(ApiData);
+      });
+      res.status(201).send(allData);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+//// Movies Api class object paradigm
+class Movies {
+  constructor(MoviesInfo) {
+    this.id = MoviesInfo.id;
+    this.title = MoviesInfo.title;
+    this.overview = MoviesInfo.overview;
+    this.average_votes = MoviesInfo.vote_average;
+    this.total_votes = MoviesInfo.vote_count;
+    this.image_url =`https://image.tmdb.org/t/p/w500${MoviesInfo.poster_path}`
+    this.popularity = MoviesInfo.popularity;
+    this.release_date = MoviesInfo.release_date;
+  }
+}
+
+//// ForeCast Api class object paradigm
+class ForeCast {
+  constructor(datetime, description) {
+    this.datetime = datetime;
+    this.description = description;
+  }
+}
+
+server.listen(PORT, () => console.log(`server Running on port ${PORT}`));
